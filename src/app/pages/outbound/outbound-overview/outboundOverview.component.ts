c187321c0ea0a6357aa44f19b67b528e59a0ec5d
bigredboots <bigredboots@gmail.com>
2022-01-04 16:29:07 -0700

    5 files modified

--- src/app/pages/outbound/outbound-overview/outboundOverview.component.ts
+++ src/app/pages/outbound/outbound-overview/outboundOverview.component.ts
@@ -3,25 +3,36 @@ import { Component, Input, OnInit, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'outbound-overview',
   templateUrl: './outboundOverview.component.html',
-  styleUrls: ['./outboundOverview.component.scss']
+  styleUrls: ['./outboundOverview.component.scss'],
 })
 export class OutboundOverviewComponent {
-  @ViewChild('partoverview') partoverview: ElementRef;
-  @ViewChild('fsiteoverview') fsiteoverview: ElementRef;
-  @ViewChild('totaldemandoverview') totaldemandoverview: ElementRef;
-  @ViewChild('flocationsoverview') flocationsoverview: ElementRef;
+  @ViewChild('outboundpartoverview') outboundpartoverview: ElementRef;
+  @ViewChild('ioutboundpartoverview') ioutboundpartoverview: ElementRef;
+  @ViewChild('outboundoverview') outboundoverview: ElementRef;
+  @ViewChild('ioutboundoverview') ioutboundoverview: ElementRef;
+  @ViewChild('foutboundlocationsoverview')
+  foutboundlocationsoverview: ElementRef;
+  @ViewChild('ifoutboundlocationsoverview')
+  ifoutboundlocationsoverview: ElementRef;
 
-  PartOverviewtoggleMoreInfo() {
-    this.partoverview.nativeElement.classList.toggle('moreinfo-active');
+  OutboundPartOverviewtoggleMoreInfo() {
+    this.outboundpartoverview.nativeElement.classList.toggle('moreinfo-active');
+    this.ioutboundpartoverview.nativeElement.classList.toggle(
+      'moreinfo-active'
+    );
   }
-  FsitetoggleMoreInfo() {
-    this.fsiteoverview.nativeElement.classList.toggle('moreinfo-active');
+  OutboundOverviewtoggleMoreInfo() {
+    this.outboundoverview.nativeElement.classList.toggle('moreinfo-active');
+    this.ioutboundoverview.nativeElement.classList.toggle('moreinfo-active');
   }
-  TotalDemandMoreInfo() {
-    this.totaldemandoverview.nativeElement.classList.toggle('moreinfo-active');
+  FOutboundLocationsMoreInfo() {
+    this.foutboundlocationsoverview.nativeElement.classList.toggle(
+      'moreinfo-active'
+    );
+    this.ifoutboundlocationsoverview.nativeElement.classList.toggle(
+      'moreinfo-active'
+    );
   }
-  FLocationsMoreInfo() {
-    this.flocationsoverview.nativeElement.classList.toggle('moreinfo-active');
-  }
+
   ngOnInit(): void {}
 }
